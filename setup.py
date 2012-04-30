56f109f17a33214b03399f2d7f0adaf843763834
herr kaste <herr.kaste@googlemail.com>
2012-05-01 01:00:32 +0200

    Push to BETA

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
 	install_requires=['useless.pipes'],
 	tests_require=['pytest', 'unittest2', 'mockito', 'cssmin'],
 	classifiers= [
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
