a7a833f5a1650fa0a4611b80607868e399271bc0
herr kaste <herr.kaste@googlemail.com>
2012-04-30 14:22:21 +0200

    Version bump to 0.0.5

--- ass/ets/__init__.py
+++ ass/ets/__init__.py
@@ -7,7 +7,7 @@
     :license: BSD, see LICENSE for more details.
 """
 
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 
 from bundles import Environment, Assets, bundle, Bundle, Manifest
 import filters as f
